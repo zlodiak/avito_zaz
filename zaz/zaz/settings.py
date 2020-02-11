1310060b7e5ea0df70e9299c3380ca8ca2a21e26
kalinin <prozaik81-2@yandex.ru>
2020-02-11 23:25:29 +0300

    banned

--- zaz/zaz/settings.py
+++ zaz/zaz/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'zaz.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'zaz (+http://www.yourdomain1.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.1
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -52,9 +52,9 @@ DOWNLOAD_DELAY = 0.1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'zaz.middlewares.ZazDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- zaz/zaz/spiders/zaz_spider.py
+++ zaz/zaz/spiders/zaz_spider.py
@@ -9,11 +9,19 @@ class ZazSpider(scrapy.Spider):
     name = "zaz"
     start_urls = ['https://www.avito.ru/rossiya/avtomobili?q=%D0%B7%D0%B0%D0%BF%D0%BE%D1%80%D0%BE%D0%B6%D0%B5%D1%86']
     host = 'https://www.avito.ru'
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+    proxy = '83.149.70.159:13011'    
 
     def parse(self, response):
         for details_page_link in response.css('div.item_table .item__line .snippet-title .snippet-link::attr(href)').getall():
             url = urljoin(self.host, details_page_link)
-            yield response.follow(url, callback=self.parse_details_page)
+            yield response.follow(
+                url, 
+                callback=self.parse_details_page, 
+                headers={"User-Agent": self.user_agent},
+                cookies=None,
+                meta={"proxy": self.proxy}
+            )
 
     def parse_details_page(self, response):
         loader = ItemLoader(item=ZazItem(), selector=response)
